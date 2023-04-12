c2fc13d61fd185e82444e3bf175dc01c710e8fb5
엄성웅 <tjddnd3116@gmail.com>
2023-04-12 14:43:17 +0900

    Merge branch 'dev' of https://github.com/42trancendence/back_end into Feature/13/User-Api

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from './util/swagger';
 import * as session from 'express-session';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  setupSwagger(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('NestJS API')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'JWT',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   app.enableCors();
   app.use(
     session({
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,7 +16,16 @@ import { UsersService } from './users.service';
 import { UserInfoDto } from './dto/user-info.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+  ApiQuery,
+  ApiNotFoundResponse,
+  ApiParam,
+} from '@nestjs/swagger';
 import { UserEntity } from './entities/user.entity';
 import { getUser } from 'src/auth/decorator/get-user.decorator';
 
@@ -25,21 +34,28 @@ import { getUser } from 'src/auth/decorator/get-user.decorator';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiBearerAuth('access-token')
   @Get('me')
   @UseGuards(AuthGuard('access-jwt'))
-  @ApiOperation({
-    summary: '내 정보 API',
-    description: '내 정보를 얻는다.',
-  })
+  @ApiOperation({ summary: '내 정보 조회' })
+  @ApiOkResponse({ description: '성공', type: UserInfoDto })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
   async getMyInfo(@getUser() user: UserEntity): Promise<UserInfoDto> {
-    return { id: user.id, name: user.name, email: user.email };
+    const userInfoDto = new UserInfoDto();
+    userInfoDto.id = user.id;
+    userInfoDto.name = user.name;
+    userInfoDto.email = user.email;
+    return userInfoDto;
   }
 
   @Get('name')
-  @ApiOperation({
-    summary: '이름 중복 확인 API',
-    description: '이름 중복 확인을 한다.',
-  })
+  @UseGuards(AuthGuard('access-jwt'))
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '이름 중복 확인' })
+  @ApiQuery({ name: 'userName', description: '중복 확인할 이름' })
+  @ApiOkResponse({ description: '성공' })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
+  @ApiNotFoundResponse({ description: '이미 존재하는 이름입니다.' })
   async checkName(@Query('userName') name: string) {
     const IsExist = await this.usersService.checkName(name);
     console.log(IsExist);
@@ -47,38 +63,27 @@ export class UsersController {
     if (IsExist) {
       throw new NotFoundException('이미 존재하는 이름입니다.');
     }
-    return { message: '사용 가능한 이름입니다.' };
   }
 
   @Get('friends')
   @UseGuards(AuthGuard('access-jwt'))
-  @ApiOperation({
-    summary: '나의 모든 친구 정보 API',
-    description: '나의 모든 친구 정보를 얻는다.',
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '나의 모든 친구 정보 조회' })
+  @ApiOkResponse({
+    description: '나의 모든 친구 정보를 얻는다',
+    type: [UserInfoDto],
   })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
   async getMyFriends(@getUser() user: UserEntity) {
     return await this.usersService.getFriendList(user);
   }
 
-  @Get('friends/:id')
-  @UseGuards(AuthGuard('access-jwt'))
-  @ApiOperation({
-    summary: '나의 친구 정보 API',
-    description: '나의 친구 정보를 얻는다.',
-  })
-  async getMyFriend(
-    @getUser() user: UserEntity,
-    @Query('id') friendId: string,
-  ) {
-    return await this.usersService.getFriends(user, friendId);
-  }
-
   @Get('accept/:id')
   @UseGuards(AuthGuard('access-jwt'))
-  @ApiOperation({
-    summary: '친구 요청 수락API',
-    description: '친구요청을 수락한다.',
-  })
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '친구 요청 수락' })
+  @ApiQuery({ name: 'id', description: '친구 요청을 수락할 유저의 id' })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
   async acceptFriend(
     @getUser() user: UserEntity,
     @Query('id') friendId: string,
@@ -88,10 +93,8 @@ export class UsersController {
 
   @Get('reject/:id')
   @UseGuards(AuthGuard('access-jwt'))
-  @ApiOperation({
-    summary: '친구 요청 거절API',
-    description: '친구요청을 거절한다.',
-  })
+  @ApiOperation({ summary: '친구 요청 거절' })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
   async rejectFriend(
     @getUser() user: UserEntity,
     @Query('id') friendId: string,
@@ -101,20 +104,18 @@ export class UsersController {
 
   @Get(':id')
   @UseGuards(AuthGuard('access-jwt'))
-  @ApiOperation({
-    summary: '유저 정보 API',
-    description: '유저의 정보를 얻는다.',
-  })
+  @ApiOperation({ summary: '유저 정보 조회' })
+  @ApiParam({ name: 'id', description: '유저 고유 ID' })
+  @ApiOkResponse({ description: '성공', type: UserInfoDto })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
   async getUserInfo(@Param('id') userId: string): Promise<UserInfoDto> {
     return this.usersService.getUserInfo(userId);
   }
 
   @Delete(':id')
   @UseGuards(AuthGuard('access-jwt'))
-  @ApiOperation({
-    summary: '친구 삭제 API',
-    description: '친구를 삭제한다.',
-  })
+  @ApiOperation({ summary: '친구 삭제' })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
   async deleteFriend(
     @getUser() user: UserEntity,
     @Param('id') friendId: string,
@@ -124,10 +125,8 @@ export class UsersController {
 
   @Post('friend')
   @UseGuards(AuthGuard('access-jwt'))
-  @ApiOperation({
-    summary: '친구 요청 API',
-    description: '친구요청을 보낸다.',
-  })
+  @ApiOperation({ summary: '친구 요청' })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
   async addFriend(@getUser() user: UserEntity, @Body('id') friendId: string) {
     await this.usersService.addFriend(user, friendId);
   }
@@ -135,10 +134,8 @@ export class UsersController {
   @Put('me')
   @UseGuards(AuthGuard('access-jwt'))
   @UsePipes(ValidationPipe)
-  @ApiOperation({
-    summary: '유저 정보 업데이트 API',
-    description: '유저의 정보를 업데이트한다.',
-  })
+  @ApiOperation({ summary: '유저 정보 수정' })
+  @ApiUnauthorizedResponse({ description: 'Invalid access token' })
   async updateUserInfo(
     @getUser() user: UserEntity,
     @Body() updateUserDto: UpdateUserDto,
