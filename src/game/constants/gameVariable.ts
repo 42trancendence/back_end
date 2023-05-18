7081c97216d59f910ac6452e0e6a2a87046a97af
guswlsakfls <guswlsakfls@gmail.com>
2023-05-18 22:24:05 +0900

    [HITFIX] 게임 스피드 조정

--- src/game/constants/gameVariable.ts
+++ src/game/constants/gameVariable.ts
@@ -30,6 +30,6 @@ export class GameVariable {
   public static readonly boundedPaddleHeight = 20;
 
   public static readonly ballRadius = 10;
-  public static readonly normalBallSpeed = 5;
-  public static readonly hardBallSpeed = 15;
+  public static readonly normalBallSpeed = 8;
+  public static readonly hardBallSpeed = 13;
 }
