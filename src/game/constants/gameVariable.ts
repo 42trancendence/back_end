0f0bc60b31d4fa050396fb088b4afd7bc5506403
Hyunjin Jo <hyujo@4r10s7.42seoul.kr>
2023-05-19 18:48:19 +0900

    [HOTFIX] 게임 옵션 수정

--- src/game/constants/gameVariable.ts
+++ src/game/constants/gameVariable.ts
@@ -11,7 +11,6 @@ export class GameVariable {
   public static readonly maxQueue = 100;
   public static readonly matchPlyers = 2;
   public static readonly maxGame = 100;
-  public static readonly maxWatchers = 5;
 
   public static readonly normalDifficulty = 'normal';
   public static readonly hardDifficulty = 'hard';
@@ -19,17 +18,17 @@ export class GameVariable {
   public static readonly normalFinalScore = 5;
   public static readonly hardFinalScore = 10;
 
-  public static readonly canvasWidth = 1024;
-  public static readonly canvasHeight = 640;
-  public static readonly paddleWidth = 10;
+  public static readonly canvasWidth = 1200;
+  public static readonly canvasHeight = 800;
+  public static readonly paddleWidth = 20;
   public static readonly paddleHeight = 200;
   public static readonly leftPaddleX = 0;
   public static readonly rightPaddleX =
     GameVariable.canvasWidth - GameVariable.paddleWidth;
-  public static readonly paddleSpeed = 10;
+  public static readonly paddleSpeed = 20;
   public static readonly boundedPaddleHeight = 20;
 
   public static readonly ballRadius = 10;
   public static readonly normalBallSpeed = 8;
-  public static readonly hardBallSpeed = 13;
+  public static readonly hardBallSpeed = 11;
 }
